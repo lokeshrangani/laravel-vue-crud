69f9b1b087a3e8ea5d77de31f0c7624f0f201b0a
lokeshrangani <57695393+lokeshrangani@users.noreply.github.com>
2023-04-12 10:14:34 +0530

    CRUD

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mt-5">
+    <div class="col-12 text-center">
+      <h1>Laravel Vue CRUD</h1>
+      <a href="/category">Create New Category</a>
+    </div>
+  </div>
+</template>
+
